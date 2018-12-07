6e1a563e0de872432b6e15a1a6a821a9b7c7d094
Bhaven Patel <3346052+bhavenp@users.noreply.github.com>
2018-12-07 11:51:26 -0500

    sparse_jacobian function works in that 1000 values can be passed in and it calculates the right output

--- optimization_tests/sparse_jacobian.py
+++ optimization_tests/sparse_jacobian.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys
 import os
-sys.path.append(os.path.join('cs207-FinalProject'))
+sys.path.append(os.path.join('..','..','cs207-FinalProject'))
 import autodiff as ad
 
 #function that takes in 1000 vals and returns a function that maps from R1000 -> R1000, so it returns a vector of 1000 values. This function should generate a sparse jacobian matrix to test newton's method on.
@@ -11,8 +11,8 @@ def sparse_jacob(x_vals):
         
     f_vals = [];
     for i, x_val in enumerate(x_vals):
-        if i == 1: #second element is log(x1)
-            func = ad.log(x_val);
+        if i == 1: #second element is ln(x1)
+            func = ad.ln(x_val);
             f_vals.append(func);
         elif i == 100: # x3 + 2*x100*exp(x100) + 4*x34
             func = x_vals[3] + 2*x_val*ad.exp(x_val) + 4*x_vals[34];
@@ -24,7 +24,7 @@ def sparse_jacob(x_vals):
             func = x_val * x_vals[450] - x_vals[888];
             f_vals.append(func);
         elif i == 887: #x887**-1 * x200/4
-            func = x_val**-1 * x_vals[200]/4;
+            func = x_val**(-1) * x_vals[200]/4;
             f_vals.append(func);
         elif i == 247: #x247**3 * ad.sin(x211)
             func = x_val**3 * ad.sin(x_vals[211]);
